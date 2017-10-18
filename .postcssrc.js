f8f4bf718c02d95cad05d08eb195469a98370b12
adinvadim <adin.vadim@gmail.com>
2017-10-19 00:05:07 +0300

    #1: Postcss loader

---
+++ .postcssrc.js
@@ -0,0 +1,15 @@
+// https://github.com/michael-ciniawsky/postcss-load-config
+
+module.exports = {
+  parser: 'postcss-scss',
+  plugins: {
+    'precss' : {},
+    'postcss-flexbugs-fixes': {},
+    'postcss-inline-svg': {},
+    'postcss-brand-colors': {},
+    'postcss-hexrgba': {},
+    'postcss-calc': {},
+    'postcss-url' : { url : 'inline' },
+    'autoprefixer': {},
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -53,6 +53,14 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader',
+        ],
+      },
     ],
   },
 };
